3193256156042194758058ee37f2ccf76f130558
akhil272 <akhil272@hotmail.com>
2023-11-22 16:01:35 +0530

    update for ngrock

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,6 +7,13 @@ const ContactPage = () => {
         <div>
           <h1 className="text-3xl font-bold">Contact Us</h1>
         </div>
+        <div className="py-4">
+          <p>
+            We are here to help and we would love to hear from you. Whether you
+            have a question, a comment or just want to chat you can reach out to
+            us through our contact form on this page, or by phone or email.
+          </p>
+        </div>
       </div>
     </div>
   );
