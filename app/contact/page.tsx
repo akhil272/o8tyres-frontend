6ec095d68f0900714d3c03ba9cdfcf29a3328dfa
akhil272 <akhil272@hotmail.com>
2023-11-22 15:55:11 +0530

    update for ngrock

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className="py-10 px-4 bg-secondary h-full flex justify-center">
+      <div className="w-full xl:w-1/2 ">
+        <div>
+          <h1 className="text-3xl font-bold">Contact Us</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
